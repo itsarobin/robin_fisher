52c23d6913e568f00df2bd8452c11b0151f8e321
Robin Fisher <robin.fisher.co@gmail.com>
2023-06-06 18:09:51 -0600

    Setup systems module
      - Create systems module with a placeholder for the movement system
      - Add poc system that queries for the `Player` component and reports
        it's ID

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use bevy::prelude::*;
 mod setup;
+mod systems;
+
+fn get_entity(
+    mut query: Query<(Entity, With<setup::actor_types::Player>)>,
+    mut commands: Commands
+) {
+    for player in &mut query {
+        let player_entity = commands.entity(player.0).id();
+        println!("Found Player! {}", player_entity.to_bits());
+    }
+}
 
 fn main() {
     App::new()
         .add_startup_system(setup::create_actors)
         .add_plugins(DefaultPlugins)
+        .add_system(get_entity)
         .run();
 }
