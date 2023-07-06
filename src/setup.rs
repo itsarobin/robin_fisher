0ef910a7249c41e5bc55f2c6b86d68d76d984c9b
Robin Fisher <robin.fisher.co@gmail.com>
2023-07-06 17:23:42 -0600

    Add a simple texture to the floor

--- src/setup.rs
+++ src/setup.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::render_resource::{TextureDimension, TextureFormat, Extent3d}};
 
 use self::actor_types::Player;
 pub mod actor_types;
@@ -6,6 +6,7 @@ pub mod actor_types;
 pub fn create_actors(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
+    mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let player_entity = commands.spawn(
@@ -23,8 +24,11 @@ pub fn create_actors(
     );
 
     commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(100.0).into()),
-        material: materials.add(Color::SILVER.into()),
+        mesh: meshes.add(shape::Plane::from_size(10.0).into()),
+        material: materials.add(StandardMaterial {
+            base_color_texture: Some(images.add(floor_texture())),
+            ..default()
+        }),
         ..default()
     }).add_child(player_entity);
 
@@ -47,4 +51,19 @@ pub fn create_actors(
             ..default()
         }
     );
+}
+
+const IMAGE_PIXELS: [u8; 16] = [255, 255, 255, 255, 0, 0, 0, 255, 0, 0, 0, 255, 255, 255, 255, 255];
+
+fn floor_texture() -> Image {
+    Image::new_fill(
+        Extent3d {
+            width: 2 as u32,
+            height: 2 as u32,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        &IMAGE_PIXELS,
+        TextureFormat::Rgba8UnormSrgb,
+    )
 }
