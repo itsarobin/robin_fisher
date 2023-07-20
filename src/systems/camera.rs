55074441ebef6ef942860127537a273600b305ab
Robin Fisher <robin.fisher.co@gmail.com>
2023-07-20 13:53:34 -0600

    Only run rotation system if right mb pressed

--- src/systems/camera.rs
+++ src/systems/camera.rs
@@ -1,20 +1,22 @@
-use bevy::prelude::{EventReader, Res, Query, Transform, Vec3};
+use bevy::prelude::*;
 use bevy::input::mouse::{MouseMotion};
 
-use crate::setup::actor_types::{Player, Camera};
+use crate::setup::actor_types::{Camera};
 
 pub fn rotate_camera(
-    player: Res<Player>,
+    mouse_input: Res<Input<MouseButton>>,
     camera: Res<Camera>,
     mut motion_event: EventReader<MouseMotion>,
     mut transforms: Query<&mut Transform>
-) {    
-    for movement in motion_event.iter(){
-        let mut new_transform = transforms.get_mut(camera.0).unwrap();
-        new_transform.rotate_y((-movement.delta.x).to_radians());
-        new_transform.rotate_x(movement.delta.y.to_radians());
-        let forward = new_transform.forward();
-
-        new_transform.look_to(forward, Vec3::Y);
+) { 
+    if mouse_input.any_pressed([MouseButton::Right]) {
+        for movement in motion_event.iter(){
+            let mut new_transform = transforms.get_mut(camera.0).unwrap();
+            new_transform.rotate_y((-movement.delta.x).to_radians());
+            new_transform.rotate_x(movement.delta.y.to_radians());
+            let forward = new_transform.forward();
+    
+            new_transform.look_to(forward, Vec3::Y);
+        }
     }
 }
